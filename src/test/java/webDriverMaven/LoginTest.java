e9b39945518c472bf0155d89dd91ca5dd6613cf6
testerak <ajay.jindle@gmail.com>
2020-09-16 15:47:06 +0100

    Added new comment to login test

--- src/test/java/webDriverMaven/LoginTest.java
+++ src/test/java/webDriverMaven/LoginTest.java
@@ -11,27 +11,29 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginTest {
-	//WebDriverManager.chromedriver().setup();
-	public static WebDriver driver; 
+	// WebDriverManager.chromedriver().setup();
+	public static WebDriver driver;
 
 	@BeforeSuite
 	public void setup() {
-	WebDriverManager.chromedriver().setup();
-	 driver = new ChromeDriver();
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
 	}
-	
+
 	@Test
 	public void doLogin() {
 		driver.get("http://www.gmail.com");
 		driver.findElement(By.id("identifierId")).sendKeys("meraselen3@gmail.com");
-		
-		driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/div[2]")).click();
-		System.out.println("Hello");
+
+		driver.findElement(By.xpath(
+				"/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/div[2]"))
+				.click();
+		System.out.println("Hello ");
 	}
+
 	@AfterSuite
 	public void tearDown() {
 		driver.quit();
 	}
-	
-	
+/* Add new comments to check if jenkins auto build works*/
 }
