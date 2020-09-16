6ef4dd2e842b9a024ea393478e978213498a61e8
testerak <ajay.jindle@gmail.com>
2020-09-16 04:56:57 +0100

    Adding my webdrivenmaven project to github

---
+++ src/test/java/webDriverMaven/LoginTest.java
@@ -0,0 +1,36 @@
+package webDriverMaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+	//WebDriverManager.chromedriver().setup();
+	public static WebDriver driver; 
+
+	@BeforeSuite
+	public void setup() {
+	WebDriverManager.chromedriver().setup();
+	 driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void doLogin() {
+		driver.get("http://www.gmail.com");
+		driver.findElement(By.id("identifierId")).sendKeys("meraselen3@gmail.com");
+		
+		driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/div[2]")).click();
+	}
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	
+}
